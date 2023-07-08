55a4b12c9e1473f4c14c0efd3b78170de7f533f2
shubham-chakravarty <shubham.chakravarty@pdgc.com>
2023-07-08 23:35:40 +0530

    Events

--- src/Content.js
+++ src/Content.js
@@ -5,11 +5,24 @@ const Content = () => {
         return names[int];
     }
 
+    const handleClick = ()=>{
+        console.log('You clicked it');
+    }
+    const handleClick1 = (name)=>{
+        console.log(`You clicked ${name}`);
+    }
+    const handleClick2 = (e)=>{
+        console.log(e);
+    }
+
     return (
         <main>
             <p>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={() => handleClick()}> Click it</button>
+            <button onClick={() => handleClick1('Shubham')}> Click it</button>
+            <button onDoubleClick={(e) => handleClick2(e)}> Click it</button>
         </main>
     )
 }
