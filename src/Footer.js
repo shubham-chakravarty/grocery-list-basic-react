538da1b859bdfbea42820eb421dd2dac4d9630cc
shubham-chakravarty <shubham.chakravarty@pdgc.com>
2023-07-09 13:30:39 +0530

    props and prop drilling

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,10 @@
-const Footer = () => {
+const Footer = (props) => {
+    const count = props.count;
     const today = new Date();
 
     return (
         <footer>
+            <p>{count} List Items</p>
             <p>Copyright &copy; {today.getFullYear()}</p>
         </footer>
     )
