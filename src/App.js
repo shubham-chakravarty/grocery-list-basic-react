53d4086a12520679f2c86b94322b601b85ac4444
shubham-chakravarty <shubham.chakravarty@pdgc.com>
2023-07-23 23:25:39 +0530

    added webserver, integrated fetch api call, handled error

--- src/App.js
+++ src/App.js
@@ -4,12 +4,37 @@ import Footer from "./Footer";
 import AddItem from "./AddItem";
 import SearchItem from "./SearchItem";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem("list")));
+  const API_URL = "http://localhost:3500/items";
+
+  const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    
+    const fetchItems = async () => {
+      try{
+        const response = await fetch (API_URL);
+        if (!response.ok)
+          throw new Error('Did not receive expected data');
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
+      }catch (err){
+        setFetchError(err.message);
+      }finally{
+        setIsLoading(false);
+      }
+    }
+    setTimeout(() => {
+      fetchItems();
+    }, 2000);
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,24 +48,23 @@ function App() {
     };
     const listItems = [...items, newEle];
     setNewItem("");
-    setAndSave(listItems);
+    updateItem(listItems);
   };
 
-  const setAndSave = (listItems) => {
+  const updateItem = (listItems) => {
     setItems(listItems);
-    localStorage.setItem("list", JSON.stringify(listItems));
   };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setAndSave(listItems);
+    updateItem(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSave(listItems);
+    updateItem(listItems);
   };
 
   return (
@@ -52,13 +76,17 @@ function App() {
         handleSubmit={handleSubmit}
       />
       <SearchItem search={search} setSearch={setSearch} />
-      <Content
-        items={items.filter((item) =>
-          item.item.toLowerCase().includes(search.toLowerCase())
-        )}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      <main>
+        {isLoading && <p>Loading Items ..</p>}
+        {fetchError && <p style={{color: 'red'}}>{`Error : ${fetchError}`}</p>}
+        {!isLoading && !fetchError && <Content
+          items={items.filter((item) =>
+            item.item.toLowerCase().includes(search.toLowerCase())
+          )}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />}
+      </main>
       <Footer count={items.length} />
     </div>
   );
